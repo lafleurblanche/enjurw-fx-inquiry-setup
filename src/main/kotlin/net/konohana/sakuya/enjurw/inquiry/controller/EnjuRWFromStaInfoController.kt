fa4364073c5656c989f93548ee2e976e85e91955
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-09 20:16:30 +0900

    20230609 ADD

--- src/main/kotlin/net/konohana/sakuya/enjurw/inquiry/controller/EnjuRWFromStaInfoController.kt
+++ src/main/kotlin/net/konohana/sakuya/enjurw/inquiry/controller/EnjuRWFromStaInfoController.kt
@@ -6,6 +6,9 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW01FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW02FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW03FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW04FromStaInfo
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -18,6 +21,30 @@ fun Route.enjuRWFromStaInfoController() {
                         SchemaUtils.create(EnjuRW01FromStaInfo)
                     }
                     call.respond(mapOf("Created:" to "EnjuRW01FromStaInfo"))
+                }
+            }
+            route("enjurw02fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW02FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW02FromStaInfo"))
+                }
+            }
+            route("enjurw03fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW03FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW03FromStaInfo"))
+                }
+            }
+            route("enjurw04fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW04FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW04FromStaInfo"))
                 }
             }
         }
