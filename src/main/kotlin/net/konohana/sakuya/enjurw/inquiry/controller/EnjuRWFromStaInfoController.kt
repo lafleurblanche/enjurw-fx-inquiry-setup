f867b2a6b4f2c67c7027468e4c324130f939670b
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-10 20:11:02 +0900

    20230609 Add

--- src/main/kotlin/net/konohana/sakuya/enjurw/inquiry/controller/EnjuRWFromStaInfoController.kt
+++ src/main/kotlin/net/konohana/sakuya/enjurw/inquiry/controller/EnjuRWFromStaInfoController.kt
@@ -9,6 +9,10 @@ import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW01FromStaInfo
 import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW02FromStaInfo
 import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW03FromStaInfo
 import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW04FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW05FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW06FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW07FromStaInfo
+import net.konohana.sakuya.enjurw.inquiry.models.fromsta.EnjuRW08FromStaInfo
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -45,6 +49,38 @@ fun Route.enjuRWFromStaInfoController() {
                         SchemaUtils.create(EnjuRW04FromStaInfo)
                     }
                     call.respond(mapOf("Created:" to "EnjuRW04FromStaInfo"))
+                }
+            }
+            route("enjurw05fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW05FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW05FromStaInfo"))
+                }
+            }
+            route("enjurw06fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW06FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW06FromStaInfo"))
+                }
+            }
+            route("enjurw07fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW07FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW07FromStaInfo"))
+                }
+            }
+            route("enjurw08fromstainfo") {
+                post {
+                    transaction {
+                        SchemaUtils.create(EnjuRW08FromStaInfo)
+                    }
+                    call.respond(mapOf("Created:" to "EnjuRW08FromStaInfo"))
                 }
             }
         }
